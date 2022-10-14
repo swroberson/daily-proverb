20b27ebce4a472815c6cbc721dae73d73f97728f
samuelwyattroberson@gmail.com <73807913+swroberson@users.noreply.github.com>
2022-10-14 15:02:55 -0500

    macOS  command no longer used

--- votd.sh
+++ votd.sh
@@ -6,8 +6,9 @@ DATE=$(date "+%d")
 PROVERB=$CURRDIR/text/"$DATE".txt
 
 # generate random number between 1 and [# of lines in PROVERB]
-LINES=$(wc -l "$PROVERB")
-NUM=$(jot -r 1 1 "$LINES")
+LINES=$(wc -l < "$PROVERB")
+NUM=$(($RANDOM % $LINES + 1))
+
 # print random line from FILE
 echo "$(sed -n "$NUM"p "$PROVERB") \
 \n ~ Proverbs "$DATE":"$NUM"" | cowsay -f $CURRDIR/scroll.cow 
