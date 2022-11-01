a0a1e41d53685a80749799353910b5f6c3c85590
samuelwyattroberson@gmail.com <73807913+swroberson@users.noreply.github.com>
2022-11-01 12:36:47 -0500

    remove date padding

--- votd.sh
+++ votd.sh
@@ -2,7 +2,8 @@
 
 # get Proverb of the day
 CURRDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-DATE=$(date "+%d")
+DATE=$(date "+%-d")
+
 PROVERB=$CURRDIR/text/"$DATE".txt
 
 # generate random number between 1 and [# of lines in PROVERB]
@@ -11,4 +12,4 @@ NUM=$(($RANDOM % $LINES + 1))
 
 # print random line from FILE
 echo "$(sed -n "$NUM"p "$PROVERB") \
-\n ~ Proverbs "$DATE":"$NUM"" | cowsay -f $CURRDIR/scroll.cow 
+\n ~ Proverbs "$DATE":"$NUM"" | cowsay -f $CURRDIR/blank.cow 
